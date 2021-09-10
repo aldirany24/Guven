0ee5c2742f8389461046befae5364b6526b46765
Mohamed Hassan <83307487+aldirany24@users.noreply.github.com>
2021-09-10 21:03:11 +0200

    Add files via upload

---
+++ js/my.js
@@ -0,0 +1,15 @@
+
+        
+var navLinks = document.getElementById("navLinks");
+
+
+function showMenu(){
+    navLinks.style.right = "0";
+    // navLinks.style.height = "200px";
+}
+function hideMenu(){
+    navLinks.style.right = "-200px";
+
+    
+}
+
